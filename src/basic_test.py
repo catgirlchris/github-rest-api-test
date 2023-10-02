908d0ce77d6db03ba888f3991bc6de6e42836fb2
Chrisfg <47301396+catgirlchris@users.noreply.github.com>
2023-10-02 13:15:30 +0200

    creating methods to interface with the api

--- src/basic_test.py
+++ src/basic_test.py
@@ -1,25 +1,49 @@
 import requests
 
-# Replace with your GitHub username and repository name
-username = 'catgirlchris'
-repo_name = 'text-adventure'
 
-# Set up the URL for the GitHub API
-api_url = f'https://api.github.com/repos/{username}/{repo_name}/branches'
+def send_request(api_url):
+    # Make a GET request to the GitHub API
+    response = requests.get(api_url)
 
-# Make a GET request to the GitHub API
-response = requests.get(api_url)
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the JSON response and return it
+        return response.json()
+    else:
+        print(f"Failed to retrieve repository info. Status code: {response.status_code}")
+        return None
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Parse the JSON response
-    branches_info = response.json()
-    
+
+def get_user(username):
+    api_url = f'https://api.github.com/users/{username}'
+    return send_request(api_url)
+
+
+def get_branches(username, repo_name):
+    api_url = f'https://api.github.com/repos/{username}/{repo_name}/branches'
+    return send_request(api_url)
+
+
+def main():
+    # Replace with your GitHub username and repository name
+    username = 'catgirlchris'
+    repo_name = 'text-adventure'
+
+    # Get user info
+    user_info = get_user(username)
+    print(f"### user {username} info ###")
+    for key, value in user_info.items():
+        print(f"--> {key}: {value}")
+
+    print()
+
+    branches_info = get_branches(username, repo_name)
+    print(f"### Repository {repo_name}@{username} branches ###")
     # Access the information you need
     for branch in branches_info:
+        print(f"### Branch {branch['name']} ###")
         for key, value in branch.items():
             print(f"--> {key}: {value}")
-            print()
 
         branch_name = branch['name']
         api_url = f"https://api.github.com/repos/{username}/{repo_name}/commits?sha={branch_name}"
@@ -28,27 +52,8 @@ if response.status_code == 200:
             commits_info = response.json()
             for commit in commits_info:
                 print("----> " + commit['sha'])
+        print()
 
-else:
-    print(f"Failed to retrieve repository info. Status code: {response.status_code}")
 
-print()
-print()
-
-# Set up the URL for the GitHub API
-api_url = f'https://api.github.com/users/{username}'
-
-# Make a GET request to the GitHub API
-response = requests.get(api_url)
-
-# Check if the request was successful
-if response.status_code == 200:
-    # Parse the JSON response
-    user_info = response.json()
-    
-    # Access the information you need
-    for key, value in user_info.items():
-        print(f"{key}: {value}")
-else:
-    print(f"Failed to retrieve repository info. Status code: {response.status_code}")
-
+if __name__ == "__main__":
+    main()
